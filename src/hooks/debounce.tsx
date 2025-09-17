8742c8f3b11fcbb74a22036697e69f5b387b6f8a
Alexandre Menand <alexandre.menand.pro@gmail.com>
2025-09-17 15:11:35 +0200

    chore: spelling value

--- src/hooks/debounce.tsx
+++ src/hooks/debounce.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react'
 
 export function useDebounce<T>(value: T, delay: number) {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value)
+  const [debounceValue, setDebounceValue] = useState<T>(value)
 
   useEffect(() => {
     const handler = setTimeout(() => {
-      setDebouncedValue(value)
+      setDebounceValue(value)
     }, delay)
 
     return () => {
@@ -13,5 +13,5 @@ export function useDebounce<T>(value: T, delay: number) {
     }
   }, [value, delay])
 
-  return debouncedValue
+  return debounceValue
 }
